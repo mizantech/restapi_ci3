63846072064c58020d4a31437abbcc058c87a2c0
mac-ohle <hostmaster@mizan.com>
2020-07-09 10:39:57 +0700

    adding MY_Controller.php

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends MX_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+} // akhir file: core/MY_Controller.php
